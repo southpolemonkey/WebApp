4fcb41adbe654c1f1607e5104b506ce6e3e430ab
rongchenxuan <rongchenxuan12345@gmail.com>
2018-02-26 14:54:54 +0800

    add if condition and for loop in html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'rong'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'I am hungary!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Hello', user=user, posts=posts)
