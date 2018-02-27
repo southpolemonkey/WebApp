b27627654e5fe8b78c6a316ddcb5fe4395f9b952
rongchenxuan <rongchenxuan12345@gmail.com>
2018-02-27 14:49:51 +0800

    login prompt, jump to main page and logout

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from werkzeug.urls import url_parse
+
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'rong'}
     posts = [
@@ -18,13 +26,43 @@ def index():
         }
     ]
 
-    return render_template('index.html', title='Hello', user=user, posts=posts)
+    return render_template('index.html', title='Hello', posts=posts)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         flash('Login requested for user {}, remember_me={}'.format(
+#             form.username.data, form.remember_me.data))
+#         return redirect(url_for('index'))
+#     return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+	# 已经登陆的用户自动回到主页
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+
+    # 验证用户名和密码
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        # 登陆名无效 或者 密码错误
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        # flask-login 插件里的函数: login_user()
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
